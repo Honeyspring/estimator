8116fb87852654a86d718312972e0f690eb7788c
Ogunsola oyindamola <honeyspringworld@gmail.com>
2020-04-22 03:01:58 +0100

    register service worker

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -16,50 +16,50 @@ const Form =({onFormSubmit,onNameChange,onAvgAgechange,onAvgDailyIncomeInUSDChan
         <legend className="f3 fw6 ph0 mh0">Please carefully Fill Out All Data Correctly</legend>
         <div className="mt3 ">
             <label className="db fw6 lh-copy f4"  htmlFor="region">Region</label>
-            <label className="db fw6 lh-copy f6"  htmlFor="name">name</label>
+            <label className="db fw6 lh-copy f6"  htmlFor="data-name">name</label>
             <label className="db fw6 lh-copy f6 FormError"  htmlFor="FormError">{FormError.FormName}</label>
             <input type="text"  className="pa2 hover-green w-100 " onChange={onNameChange}  name="region-name"  id=" data-name " required="required" data-name />
            
             </div>
          <div className="mt3 ">
-            <label className="db fw6 lh-copy f6"  htmlFor="avgAge">Average Age</label>
+            <label className="db fw6 lh-copy f6"  htmlFor="data-avg-age">Average Age</label>
             <label className="db fw6 lh-copy f6 FormError"  htmlFor="FormError">{FormError.FormAvgAge}</label>
             <input className="b pa2 hover-green w-100" type="number" onChange={onAvgAgechange}  name="region-avgAge"  id=" data-avg-age" required="required" data-avg-age/>
         </div>
          <div className="mt3 ">
-            <label className="db fw6 lh-copy f6"  htmlFor="avgDailyIncomeInUSD">Average Daily Income In USD</label>
+            <label className="db fw6 lh-copy f6"  htmlFor="data-avg-daily-income-in-usd">Average Daily Income In USD</label>
              <label className="db fw6 lh-copy f6 FormError"  htmlFor="FormError">{FormError.FormAvgDailyIncomeInUSD }</label> 
             <input className="b pa2 hover-green w-100" type="number" onChange={onAvgDailyIncomeInUSDChange} name="region-avgDailyIncomeInUSD"  id="data-avg-daily-income-in-usd" required="required" data-avg-daily-income-in-usd/>
         </div>
          <div className="mt3 ">
-            <label className="db fw6 lh-copy f6"  htmlFor="avgDailyIncomePopulation">Average Daily Income Population</label>
+            <label className="db fw6 lh-copy f6"  htmlFor="data-avgDaily-income-population">Average Daily Income Population</label>
              <label className="db fw6 lh-copy f6 FormError"  htmlFor="FormError">{FormError.FormAvgDailyIncomePopulation}</label> 
             <input className="b pa2 hover-green w-100" type="number" onChange={onAvgDailyIncomePopulationChange} name="region-avgDailyIncomePopulation"  id=" data-avgDaily-income-population" required="required"data-avg-daily-income-population/>
         </div>
          <div className="mt3 ">
-            <label className="db fw6 lh-copy f6"  htmlFor="periodType">Period Type</label>
+            <label className="db fw6 lh-copy f6"  htmlFor="data-period-type">Period Type</label>
              <label className="db fw6 lh-copy f6 FormError"  htmlFor="FormError">{FormError.FormPeriodType }</label> 
             <input className="b pa2  radio hover-green" type="radio" onChange={onPeriodTypeSelect}  name="periodType" value="Daily" id="data-period-type-Daily" required="required" data-period-type-days/>Daily
               <input className="b pa2 radio   hover-green " type="radio" onChange={onPeriodTypeSelect}  name="periodType" value="Weekly" id="data-period-type-Weekly" required="required" data-period-type-weeks/>Weekly
                 <input className="b pa2 radio hover-green " type="radio" onChange={onPeriodTypeSelect}  name="periodType" value="Monthly" id="data-period-type-Monthly" required="required" data-period-type-months/>Monthly
         </div>
         <div className="mv3 ">
-            <label className="db fw6 lh-copy f6 "  htmlFor="timeToElapse" >Time To Elapse </label> 
+            <label className="db fw6 lh-copy f6 "  htmlFor="data-time-to-elapse" >Time To Elapse </label> 
              <label className="db fw6 lh-copy f6 FormError"  htmlFor="FormError">{FormError.FormTimeToElapse }</label>  
             <input className="b pa2 hover-green w-100" type="number"  onChange={onTimeToElapseChange} name="timeToElapse"  id=" data-time-to-elapse " required="required" data-time-to-elapse/>
         </div>
         <div className="mv3 ">
-            <label className="db fw6 lh-copy f6 "  htmlFor="reportedCases" >Reported Cases</label>
+            <label className="db fw6 lh-copy f6 "  htmlFor="data-reported-cases" >Reported Cases</label>
              <label className="db fw6 lh-copy f6 FormError"  htmlFor="FormError">{FormError.ReportedCases }</label>  
             <input className="b pa2 hover-green w-100" type="number"  onChange={onReportedCasesChange} name="reportedCases"  id="data-reported-cases " required="required" data-reported-cases/>
         </div>
          <div className="mv3 ">
-            <label className="db fw6 lh-copy f6 "  htmlFor="population" >Population</label> 
+            <label className="db fw6 lh-copy f6 "  htmlFor="data-population" >Population</label> 
              <label className="db fw6 lh-copy f6 FormError"  htmlFor="FormError">{FormError.FormTimeToElapse }</label>      
             <input className="b pa2 hover-green w-100" type="number"  onChange={onPopulationChange} name=" population"  id="data-population" required="required" data-population/>
         </div>
          <div className="mv3 ">
-            <label className="db fw6 lh-copy f6 "  htmlFor="totalHospital" >Total Hospital Beds</label>  
+            <label className="db fw6 lh-copy f6 "  htmlFor="data-total-hospital-beds" >Total Hospital Beds</label>  
              <label className="db fw6 lh-copy f6 FormError"  htmlFor="FormError">{FormError.FormTotalHospitalBeds }</label>   
             <input className="b pa2 hover-green w-100" type="number"  onChange={onTotalHospitalBedsChange} name="totalHospitalBeds" id=" data-total-hospital-beds " required="required" data-total-hospital-beds />
         </div>
